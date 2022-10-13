078199f2b3c8074d8fae0b89d7729d131b7c3fbb
hmingji <hmingji@hotmail.com>
2022-10-13 14:42:29 +0800

    clear console log

--- src/hooks/useMovieListQuery.ts
+++ src/hooks/useMovieListQuery.ts
@@ -13,13 +13,7 @@ export const useMovieListQuery = (category: string, isEnabled: boolean) => {
     ({ pageParam = 1 }) => fetchMovieList(pageParam, category),
     {
       getNextPageParam: (lastPage) => {
-        console.log(
-          `page: ${lastPage.page}, total pages: ${lastPage.total_pages} `
-        );
-        if (lastPage.page < lastPage.total_pages) {
-          console.log(`returning ${lastPage.page + 1}`);
-          return lastPage.page + 1;
-        }
+        if (lastPage.page < lastPage.total_pages) return lastPage.page + 1;
         return undefined;
       },
       refetchOnWindowFocus: false,
